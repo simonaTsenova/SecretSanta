249f3d01a3d47e686b7b75f9eecc178bf900bfee
Simona Tsenova <simona.tsenova17@gmail.com>
2018-06-27 22:48:05 +0300

    Add users service tests

---
+++ SecretSanta.Tests/SecretSanta.Services/UsersServiceTests/GetAllUsers_Should.cs
@@ -0,0 +1,67 @@
+﻿using Moq;
+using NUnit.Framework;
+using SecretSanta.Data.Contracts;
+using SecretSanta.Models;
+using SecretSanta.Models.Enumerations;
+using SecretSanta.Services;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SecretSanta.Tests.SecretSanta.Services.UsersServiceTests
+{
+    [TestFixture]
+    public class GetAllUsers_Should
+    {
+        private Mock<IEfRepository<User>> repositoryMock;
+        private UserService service;
+        private IEnumerable<User> users;
+        private IEnumerable<User> expectedUsers;
+        private const int SKIP = 0;
+        private const int TAKE = 0;
+        private const string SEARCH = "es";
+
+        [Test]
+        public void CallRepositoryAll_WhenInvoked()
+        {
+            service.GetAllUsers(SKIP, TAKE, OrderType.Ascending, SEARCH);
+
+            this.repositoryMock.Verify(r => r.All, Times.Once);
+        }
+
+        [Test]
+        public void ReturnCorrectUsersCollection_WhenOrderTypeIsAscending()
+        {
+            var result = service.GetAllUsers(SKIP, TAKE, OrderType.Ascending, SEARCH);
+
+            Assert.AreEqual(this.expectedUsers.Count(), result.Count());
+            CollectionAssert.AreEqual(this.expectedUsers, result);
+        }
+
+        [Test]
+        public void ReturnCorrectUsersCollection_WhenOrderTypeIsDescending()
+        {
+            var result = service.GetAllUsers(SKIP, TAKE, OrderType.Descending, SEARCH);
+
+            Assert.AreEqual(this.expectedUsers.Count(), result.Count());
+            CollectionAssert.AreEqual(this.expectedUsers.OrderByDescending(u => u.DisplayName), result);
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            this.users = new List<User>()
+            {
+                new User("Test 1", "Test 1", "Test 1", "Test 1", "Test 1"),
+                new User("Sample", "Sample", "Sample", "Sample", "Sample"),
+                new User("Test 2", "Test 2", "Test 2", "Test 2", "Test 2")
+            };
+
+            this.expectedUsers = this.users.Where(u => u.DisplayName.Contains(SEARCH));
+
+            this.repositoryMock = new Mock<IEfRepository<User>>();
+            this.repositoryMock.Setup(r => r.All).Returns(this.users.AsQueryable());
+
+            this.service = new UserService(repositoryMock.Object);
+        }
+    }
+}
---
+++ SecretSanta.Tests/SecretSanta.Services/UsersServiceTests/GetUserById_Should.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using NUnit.Framework;
+using SecretSanta.Common;
+using SecretSanta.Common.Exceptions;
+using SecretSanta.Data.Contracts;
+using SecretSanta.Models;
+using SecretSanta.Services;
+
+namespace SecretSanta.Tests.SecretSanta.Services.UsersServiceTests
+{
+    [TestFixture]
+    public class GetUserById_Should
+    {
+        private Mock<IEfRepository<User>> repositoryMock;
+        private UserService service;
+        private IEnumerable<User> users;
+        private User expectedUser;
+        private const string ID = "1";
+
+        [Test]
+        public void ReturnCorrectUser_WhenProvidedValidID()
+        {
+            var result = service.GetUserById(ID);
+
+            Assert.AreEqual(expectedUser, result);
+        }
+
+        [Test]
+        public void ThrowItemNotFoundException_WhenProvidedInvalidUsername()
+        {
+            var ex = Assert.Throws<ItemNotFoundException>(() => service.GetUserById("2"));
+            Assert.AreEqual(Constants.USER_ID_NOT_FOUND, ex.Message);
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            this.users = new List<User>()
+            {
+                new User() { Id = "1" }, 
+            };
+
+            this.expectedUser = users.First(u => u.Id.Equals(ID));
+
+            this.repositoryMock = new Mock<IEfRepository<User>>();
+            this.repositoryMock.Setup(r => r.All).Returns(this.users.AsQueryable());
+
+            this.service = new UserService(repositoryMock.Object);
+        }
+    }
+}
